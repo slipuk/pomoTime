f89e109c256de4ddfad8e2fba0cd9b782119bed8
slipuk <205234930+slipuk@users.noreply.github.com.>
2025-06-21 22:09:21 +0300

    add function that chages modes
    function that changes mode - method to change mode between "work", "shortBreak" and "longBreak". After completing full cycle returns to the start of the cycle

--- src/renderer.js
+++ src/renderer.js
@@ -1,21 +1,25 @@
 let timer = null;
 let timeLeft = 25 * 60;
+let currentMode = "work"; //"shortBreak", 'longBreak'
+let workSectionCount = 0; // from 0 to 3
 
 function startTimer() {
-  if (timer !== null) return;
+  if (timer !== null) return; //for preventing doulbe launch
   timeLeft--;
   updateDisplay(timeLeft);
 
+  // actual loop for timer
   timer = setInterval(() => {
     timeLeft--;
     updateDisplay(timeLeft);
 
+    // to stop timer when 00:00 to laucnh other functions
     if (timeLeft <= 0) {
       clearInterval(timer);
       timer = null;
       onTimerEnd();
     }
-  }, 1000);
+  }, 0);
 }
 
 function stopTimer() {
@@ -29,13 +33,41 @@ function resetTimer() {
   updateDisplay(timeLeft);
 }
 
+// updating display
 function updateDisplay(seconds) {
   const mins = Math.floor(seconds / 60);
   const secs = seconds % 60;
+
+  //to change actual text
   document.getElementById('timerDisplay').textContent =
     `${mins.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
+  //this interpolation method always leaves two numbers on each
+  //timer section like 01:02
 }
 
-function onWorkTimerEnd() {
-  new Audio('assets/.mp3').play();
+function onTimerEnd() {
+  if (currentMode === "work" && workSectionCount !== 3) {
+    currentMode = "shortBreak";
+    stopTimer();
+    workSectionCount++;    
+  }
+  else if (currentMode === "work" && workSectionCount === 3) {
+    currentMode = "longBreak";
+    workSectionCount = 0;
+    stopTimer();
+  }
+  else {
+    currentMode = "work";
+    stopTimer();
+  }
+  changeCurrentMode();
 }
+
+function changeCurrentMode() {
+  if (currentMode === "work") timeLeft = 25 * 60;
+  else if (currentMode === "shortBreak") timeLeft = 5 * 60;
+  else if (currentMode === "longBreak") timeLeft = 30 * 60;
+  updateDisplay(timeLeft);
+}
+
+update
