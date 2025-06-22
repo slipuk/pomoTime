0c96bb3a8e688b1b91e5a58b44e0555649e357ed
slipuk <205234930+slipuk@users.noreply.github.com.>
2025-06-22 16:57:56 +0300

    fix style, add new feature like progress bar

--- renderer.js
+++ renderer.js
@@ -19,7 +19,7 @@ function startTimer() {
       timer = null;
       onTimerEnd();
     }
-  }, 10);
+  }, 0);
 }
 
 function stopTimer() {
@@ -29,7 +29,9 @@ function stopTimer() {
 
 function resetTimer() {
   stopTimer();
-  timeLeft = 25 * 60;
+  if (currentMode === "work") timeLeft = 25 * 60;
+  else if (currentMode === "shortBreak") timeLeft = 5 * 60;
+  else if (currentMode === "longBreak") timeLeft = 30 * 60;
   updateDisplay(timeLeft);
 }
 
@@ -39,15 +41,18 @@ function updateDisplay(seconds) {
   const secs = seconds % 60;
 
   //to change actual text
-  document.getElementById('timerDisplay').textContent =
-    `${mins.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
   //this interpolation method always leaves two numbers on each
   //timer section like 01:02
+  document.getElementById('timerDisplay').textContent =
+    `${mins.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
 
   // to see what mode is currently in progress
   document.getElementById("modeIndicator").textContent = 
     currentMode === "work" ? "Work" :
     currentMode === "shortBreak" ? "Short Break" : "Long Break";
+
+  // to track how much time left
+  document.getElementById("progressBarFill").style.width = `${progressBarPersentage()}%`;
 }
 
 // to chage current mode
@@ -78,4 +83,11 @@ function changeCurrentMode() {
 
 function playSound() {
   new Audio('assets/clock_alarm.mp3').play();
+}
+
+function progressBarPersentage() {
+  const totalTime = currentMode === "work" ? 25 * 60 :
+                    currentMode === "shortBreak" ? 5 * 60 : 30 * 60;
+  const persent = ( (totalTime - timeLeft) / totalTime ) * 100;
+  return persent;
 }
